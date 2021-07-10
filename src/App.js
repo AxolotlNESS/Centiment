553641eeda24c28cafefc0f838637c3852d64fb9
Sean Grace <seanmgrace@gmail.com>
2021-07-10 14:19:12 -0700

    initial backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Feed from "./components/Feed.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentFeed: [
+        { sender: "Spencer", text: "thank you", recipient: "Nick" },
+      ],
+      msg: "",
+      points: 0,
+      user: "",
+      rec: "",
+      potRec: ["Sean", "Emma", "Spencer", "Nick"],
+    };
+  }
+
+  render() {
+    const potRecc = [];
+    this.state.potRec.forEach((el) => {
+      potRecc.push(<option value={el}> {el} </option>);
+    });
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+        <div className="header">
+          <h1>Welcome to Centiment</h1>
+          <form>
+            <label> Message</label>
+            <input type="text"></input>
+            <label> Points </label>
+            <select name="points">
+              <option value="5"> 5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+            </select>
+            <label> Recipients </label>
+            <select name="Recipients">
+              {potRecc}
+            </select>
+            <button name="submit" onClick= "heli">
+              Submit 
+            </button>
+          </form>
+        </div>
+        <div className="feed">
+          <Feed feedItems={this.state.currentFeed}></Feed>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Feed extends Component {
+  render() {
+    const feedItems = [];
+    for (let item in this.props.feedItems) {
+      feedItems.push(<FeedItem attributes={this.props.feedItems[item]} />);
+    }
+    return (
+      <div>
+        {feedItems}
+      </div>
+    )
+  }
+}
+
+class FeedItem extends Component {
+  render() {
+    // sender text reciepient
+    return (
+      <div>
+        <h3>{this.props.attributes.sender}</h3>
+        <div>
+          <p>
+            {this.props.attributes.text} to {this.props.attributes.recipient}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Feed;
