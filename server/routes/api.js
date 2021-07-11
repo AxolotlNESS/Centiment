d4a4ba29a82e6d39c3821093e458e82422f52bbb
Emma Czech <emmaczech@Emmas-MacBook-Pro.local>
2021-07-11 16:07:31 -0600

    recip query and feed query complete

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userController.js');
 
 const router = express.Router();
 
 //userController.getRecipients,
-// console.log(JSON.stringify(userController.getRecipients));
+// console.log(userController.getRecipients());
 router.get('/', userController.getRecipients, (req, res) =>
   //go back and console log res.locals.recipients
   res.status(200).json([...res.locals.recipients])
 );
+
+// console.log(userController.getFeed());
+router.get('/feed', userController.getFeed, (req, res) =>
+  res.status(200).json([...res.locals.feed])
+);
+
+// router.post('/')
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const express = require('express');
 const apiRouter = require('./routes/api');
+// const userController = require('./userController.js');
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 
 app.use(express.static(path.resolve(__dirname, '../src')));
 
+// app.get('/', userController.getRecipients, (req, res) =>
+//   //go back and console log res.locals.recipients
+//   res.status(200).json([...res.locals.recipients])
+// );
 app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
